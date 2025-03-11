cba0274cf27ce45bc08360f8629c6af034b18261
hisyammmmm <152193241+hisyammmmm@users.noreply.github.com>
2025-03-11 16:53:54 +0700

    tambah IP

--- tugas2/backend/config/database.js
+++ tugas2/backend/config/database.js
@@ -1,7 +1,7 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize('db_notes', 'root', '', {
-    host: 'localhost', //klo udah deploy pake ip server database
+const db = new Sequelize('db_notes', 'root', 'bebas', {
+    host: '34.69.221.185', //klo udah deploy pake ip server database
     dialect: 'mysql'
 })
 
